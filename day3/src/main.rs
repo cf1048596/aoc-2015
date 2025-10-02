d212678ce9bbeaebd2fe0a0d7dcaf00ab92ac4d7
cf1048596 <119711689+cf1048596@users.noreply.github.com>
2025-10-02 22:07:32 +1300

    ugly solution but works part 2 day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,31 +6,50 @@ fn main() {
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
     let mut current_coords: (i32, i32) = (0, 0);
+    let mut mirrored_current_coords: (i32, i32) = (0, 0);
     let input = std::fs::read_to_string(input).unwrap().to_lowercase();
     let parsed = input.trim();
 
+    let mut which_santa: bool = false;
+
+
     parsed.chars().for_each(|c| {
-        match c {
-            '>' => {
+        match (c, which_santa) {
+            ('>', false) => {
                 current_coords.0 += 1;
             }
-            '<' => {
+            ('<', false) => {
                 current_coords.0 -= 1;
             }
-            '^' => {
+            ('^', false) => {
                 current_coords.1 += 1;
             }
-            'v' => {
+            ('v', false) => {
                 current_coords.1 -= 1;
+            }
+            ('>', true) => {
+                mirrored_current_coords.0 += 1;
+            }
+            ('<', true) => {
+                mirrored_current_coords.0 -= 1;
+            }
+            ('^', true) => {
+                mirrored_current_coords.1 += 1;
+            }
+            ('v', true) => {
+                mirrored_current_coords.1 -= 1;
             }
             _ => (),
         }
+        which_santa = !which_santa;
         if !visited.contains(&current_coords) {
             visited.insert(current_coords);
             houses_visited += 1;
+        } else if !visited.contains(&mirrored_current_coords) {
+            visited.insert(mirrored_current_coords);
+            houses_visited += 1;
         }
     });
-
 
     println!("{houses_visited}");
 }
